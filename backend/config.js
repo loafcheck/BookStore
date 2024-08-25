7aa51654257a9a40a87b4c4d93c3d089828dbcbe
loafcheck <rachelskim.contact@gmail.com>
2024-08-24 17:01:30 -0700

    create book model with mongoose

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+'mongodb+srv://root:root@cluster0.38ffn.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
---
+++ backend/index.js
@@ -0,0 +1,48 @@
+import express, { response } from "express";
+import {PORT, mongoDBURL} from "./config.js";
+import mongoose from "mongoose";
+
+const app = express();
+
+app.get('/', (request, response)=> {
+    console.log(request);
+    return response.status(234).send('Welcome to Mern Stack Tutorial');
+});
+
+app.post('/books', async(request,response)=> {
+    try {
+        if (
+            !request.body.title ||
+            !request.body.author ||
+            !request.body.publishYear
+        ) {
+            return response.status(400).send ({
+                message: 'Send all required fields: title, author, publishYear'
+            }); 
+        }
+        const newBook = {
+            title: request.body.title,
+            author: request.body.author,
+            publishYear: request.body.publishYear
+        }
+        const book = await Book.create(newBook);
+        
+        return response.status(201).send(book);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({message:error.message})
+    }
+})
+
+mongoose
+    .connect(mongoDBURL)
+    .then(()=>{
+        console.log('App connected to database');
+        app.listen(PORT, ()=> {
+            console.log(`App is listening to port : ${PORT}`);
+        });
+    })
+    .catch((error)=> {
+        console.log(error);
+    });
