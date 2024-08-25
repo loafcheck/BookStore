2f9761baa6d75e56005c70129b67f2572b3e8a90
loafcheck <rachelskim.contact@gmail.com>
2024-08-24 21:03:09 -0700

    Update a Book with mongoose

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,61 @@ app.post('/books', async(request,response)=> {
         console.log(error.message);
         response.status(500).send({message:error.message})
     }
-})
+});
+// Route for Get All Books from database
+app.get('/books', async (request, response)=> {
+    try {
+        const books = await Book.find({});
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+// Route for Get One Book from database
+app.get('/books/:id', async (request, response)=> {
+    try {
+        const {id} = request.params;
+
+        const book = await Book.findById(id);
+
+        return response.status(200).json(book);
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
+
+//Route for Update a Book
+app.put('/books/:id', async (request, response)=> {
+    try {
+        if (
+        !request.body.title ||
+        !request.body.author ||
+        !request.body.publishYear
+    ) {
+        return response.status(400).send ({
+            message: 'Send all required fields: title, author, publishYear'
+        }); 
+    }
+    const {id} = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+
+    if(!result) {
+        return response.status(404).json({message: 'Book not found'});
+    }
+
+    return response.status(200).send({message: 'Book updated successfully'})
+     
+    } catch(error){
+        console.log(error.message);
+        response.status(500).send({message:error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
